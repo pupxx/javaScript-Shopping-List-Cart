ba7495bdacddc5530c7cbfc692ceb9adbc5d884a
Cheryln <cherylnbarber@gmail.com>
2017-10-16 18:58:29 -0700

    Shopping List / Cart built with vanilla javaScript

---
+++ main.js
@@ -0,0 +1,45 @@
+let formContents = document.querySelector('.listInput')
+
+formContents.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    let inputText = document.querySelector('#item-input').value;
+
+   addAndRemove(inputText) 
+   document.querySelector('.listInput').reset()   
+});
+
+
+
+
+
+function addAndRemove(input) {
+    var ul = document.querySelector('.shopping-list-items');
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(input))
+    ul.appendChild(li)
+
+    var button = document.createElement('button')
+    button.innerHTML = 'Add to Cart';
+    button.setAttribute('class', 'add-to-cart')
+    li.appendChild(button)
+    
+    addToCart(button, ul, li)
+    
+}
+
+
+function addToCart(button, ul, li){
+    button.addEventListener('click', function (e) {
+        if (button.innerHTML === 'Add to Cart') {
+            let listItem = this.parentNode.childNodes[0];
+            let cartUl = document.querySelector('.shopping-cart-list')
+            let cartLi = document.createElement('li')
+            cartLi.appendChild(document.createTextNode(listItem))
+            cartUl.appendChild(li)
+            button.innerHTML = 'return to list'
+        } else {
+            ul.appendChild(li);
+            this.innerHTML = 'Add to Cart';
+        }
+    })
+}
